edc36952fc057b55d00b5941c7f85adffdddd50e
Aidan Cox <aidan2002@gmail.com>
2022-06-29 13:08:51 -0400

    Create Vue Frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import sessionManager from "@/store/modules/session_manager";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
   modules: {
+    sessionManager
   }
 })
